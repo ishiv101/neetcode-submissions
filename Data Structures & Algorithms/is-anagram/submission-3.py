68f315ea59b7e625503c5ad8080ef35bc90b7947
Ishi Varshney <ishiv@unc.edu>
2026-08-05 16:59:52 -0400

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        else:
+            for char in s:
+                if char in t:
+                    t = t.replace(char, "", 1)
+                else:
+                    return False
+        return True
+        
